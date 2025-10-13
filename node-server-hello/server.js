4319081839e7f0ebcd7fedf33d2e2845e48f6c19
salmaarayyan <salmaarayyan@gmail.com>
2025-10-13 12:56:04 +0700

    Update tugas 2

--- node-server-hello/server.js
+++ node-server-hello/server.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+ 	const express = require('express');
+ 	const cors = require('cors'); 
+ 	const app = express();
+ 	const PORT = 3001;
+ 	
+ 	// Middleware
+ 	app.use(cors()); 
+ 	app.use(express.json()); 
+ 	app.use((req, res, next) => {
+ 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+ 	  next();
+ 	});
+ 	
+ 	app.get('/', (req, res) => {
+ 	  res.send('Home Page for API');
+ 	});
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
-});
+  	const ruteBuku = require("./routes/books");
+ 	app.use('/api/books', ruteBuku);
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+ 	
+ 	app.listen(PORT, () => {
+ 	  console.log(`Express server running at http://localhost:${PORT}/`);
+ 	});
---
+++ tugas/Tugas2.md
@@ -0,0 +1,13 @@
+# Tugas 2
+
+Tampilan Endpoint Create Book:
+![Tampilan endpoint create](ss/endpoint_create.png)
+
+Tampilan Endpoint Read Book:
+![Tampilan endpoint read](ss/endpoint_read.png)
+
+Tampilan Endpoint Update Book:
+![Tampilan endpoint update](ss/endpoint_update.png)
+
+Tampilan Endpoint Delete Book:
+![Tampilan endpoint delete](ss/endpoint_delete.png)
