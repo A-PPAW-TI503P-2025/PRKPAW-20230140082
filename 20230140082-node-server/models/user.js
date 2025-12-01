5cfceb792211a6690458c8ab267620dbd41312df
salmaarayyan <salmaarayyan@gmail.com>
2025-12-01 12:31:24 +0700

    Praktikum Integrasi Final Full-Stack, Relasi Database, dan Geolocation

--- 20230140082-node-server/models/user.js
+++ 20230140082-node-server/models/user.js
@@ -9,9 +9,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      User.hasMany(models.Presensi, { foreignKey: 'userId' });
-    }
+      static associate(models) {
+        User.hasMany(models.Presensi, { 
+          foreignKey: 'userId', 
+          as: 'presensi' 
+        });
+      }
+
   }
   User.init({
     nama: {
---
+++ tugas/Tugas9.md
@@ -0,0 +1,13 @@
+# Tugas 9
+
+Tampilan halaman presensi-check in:
+![alt text](ss/prak9_checkin.png)
+
+Tampilan halaman presensi-check out:
+![alt text](ss/prak9_checkout.png)
+
+Tampilan halaman laporan admin:
+![alt text](ss/prak9_laporan.png)
+
+Tampilan database tabel presensi:
+![alt text](ss/prak9_db.png)
